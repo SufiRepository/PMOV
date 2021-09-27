a623949bac55ed98aacea745b80afbbb0c2def79
farez <farez@mindwave.my>
2021-09-27 15:09:39 +0800

    helpdesks new update

--- app/Http/Controllers/Api/HelpdesksController copy.php
+++ app/Http/Controllers/Api/HelpdesksController copy.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use Illuminate\Support\Facades\Auth;
 
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
-use App\Http\Transformers\SelectlistTransformer;
 use App\Http\Transformers\HelpdesksTransformer;
+use App\Http\Transformers\SelectlistTransformer;
 use App\Models\Helpdesk;
-use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -16,79 +16,44 @@ class HelpdesksController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @author [farez] [<farez@mindwave.my>]
      * @since [v4.0]
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        $this->authorize('view', Helpdesk::class);
+        // $this->authorize('view', Helpdesk::class);
         $allowed_columns = [
-            'id',
-            'name',
-            'client_name',
-            'client_phone',
-            'client_email',
-            'client_address',
-            'priority',
-            'status',
-            'due_date',
-            'description',
-            'created_at',
-            'updated_at',
-            'deleted_at',
-            'engineer',
-            'solution',
-            'solution_status',
-            'responded_date',
-            'user_id',
-            'location_id',
-            'company_id',
-            'project_id',
-            'team_member',
-            
-        ];
-        
-        $helpdesks = Helpdesk::select(
-            array(
-                'id',
-                'name',
-                'client_name',
-                'client_phone',
-                'client_email',
-                'client_address',
-                'priority',
-                'status',
-                'due_date',
-                'description',
-                'created_at',
-                'updated_at',
-                'deleted_at',
-                'engineer',
-                'solution',
-                'solution_status',
-                'responded_date',
-                'user_id',
-                'location_id',
-                'company_id',
-                'project_id',
-                'team_member',
-            )
-        );
-            //     ->withCount('assets as assets_count')
-            // ->withCount('licenses as licenses_count')
-            // ->withCount('accessories as accessories_count');
+                            'id',
+                            'name',
+                            'notes',
+                            'created_at',
+                            'updated_at',
+                            'deleted_at',
+                            'project_id',
+                            'company_id',
+                            'user_id',
+                        ];
 
-            $helpdesks = Company::scopeCompanyables($helpdesks);
+        // $helpdesks = Helpdesk::select(  array('id','name','notes','created_at','updated_at', 'deleted_at'));
+        $user_company_id = Auth::user()->company_id;
+
+        $helpdesks = Helpdesk::where('company_id', $user_company_id)->get();
+        // )->withCount('assets as assets_count')->withCount('licenses as licenses_count')->withCount('consumables as consumables_count')->withCount('accessories as accessories_count');
+
+        if ($request->input('deleted')=='true') {
+            $helpdesks->onlyTrashed();
+        }
 
         if ($request->filled('search')) {
             $helpdesks = $helpdesks->TextSearch($request->input('search'));
         }
 
-        
         if ($request->filled('project_id')) {
             $helpdesks->where('project_id','=',$request->input('project_id'));
         }
+
+
         // Set the offset to the API call's offset, unless the offset is higher than the actual count of items in which
         // case we override with the actual count, so we should return 0 items.
         $offset = (($helpdesks) && ($request->get('offset') > $helpdesks->count())) ? $helpdesks->count() : $request->get('offset', 0);
@@ -98,11 +63,12 @@ class HelpdesksController extends Controller
 
         $order = $request->input('order') === 'asc' ? 'asc' : 'desc';
         $sort = in_array($request->input('sort'), $allowed_columns) ? $request->input('sort') : 'created_at';
-        $helpdesks->orderBy($sort, $order);
+        // $helpdesks->orderBy($sort, $order);
 
         $total = $helpdesks->count();
-        $helpdesks = $helpdesks->skip($offset)->take($limit)->get();
-        return (new HelpdesksTransformer)->transformHelpdesks($helpdesks, $total);
+        // $helpdesks = $helpdesks->skip($offset)->take($limit)->get();
+        return (new HelpdesksTransformer)->transformHelpdesks($helpdesks);
+        // return (new ManufacturersTransformer)->transformManufacturers($manufacturers, $total);
     }
 
 
@@ -138,7 +104,7 @@ class HelpdesksController extends Controller
     public function show($id)
     {
         $this->authorize('view', Helpdesk::class);
-        $helpdesk = Helpdesk::findOrFail($id);
+        $helpdesk = Helpdesk::withCount('assets as assets_count')->withCount('licenses as licenses_count')->withCount('consumables as consumables_count')->withCount('accessories as accessories_count')->findOrFail($id);
         return (new HelpdesksTransformer)->transformHelpdesk($helpdesk);
     }
 
@@ -175,25 +141,17 @@ class HelpdesksController extends Controller
      */
     public function destroy($id)
     {
+
         $this->authorize('delete', Helpdesk::class);
-        $helpdesk = Helpdesk::with('asset_maintenances', 'assets', 'licenses')->withCount('asset_maintenances as asset_maintenances_count','assets as assets_count', 'licenses as licenses_count')->findOrFail($id);
+        $helpdesk = Helpdesk::findOrFail($id);
         $this->authorize('delete', $helpdesk);
 
-
-        if ($helpdesk->assets_count > 0) {
-            return response()->json(Helper::formatStandardApiResponse('error', null,  trans('admin/helpdesks/message.delete.assoc_assets', ['asset_count' => (int) $helpdesk->assets_count])));
+        if ($helpdesk->isDeletable()) {
+            $helpdesk->delete();
+            return response()->json(Helper::formatStandardApiResponse('success', null,  trans('admin/helpdesks/message.delete.success')));
         }
 
-        if ($helpdesk->asset_maintenances_count > 0) {
-            return response()->json(Helper::formatStandardApiResponse('error', null,  trans('admin/helpdesks/message.delete.assoc_maintenances', ['asset_maintenances_count' => $helpdesk->asset_maintenances_count])));
-        }
-
-        if ($helpdesk->licenses_count > 0) {
-            return response()->json(Helper::formatStandardApiResponse('error', null, trans('admin/helpdesks/message.delete.assoc_licenses', ['licenses_count' => (int) $helpdesk->licenses_count])));
-        }
-
-        $helpdesk->delete();
-        return response()->json(Helper::formatStandardApiResponse('success', null,  trans('admin/helpdesks/message.delete.success')));
+        return response()->json(Helper::formatStandardApiResponse('error', null,  trans('admin/helpdesks/message.assoc_users')));
 
     }
 
@@ -211,11 +169,11 @@ class HelpdesksController extends Controller
         $helpdesks = Helpdesk::select([
             'id',
             'name',
-            'image',
+            'issue',
         ]);
 
         if ($request->filled('search')) {
-            $helpdesks = $helpdesks->where('helpdesks.name', 'LIKE', '%'.$request->get('search').'%');
+            $helpdesks = $helpdesks->where('name', 'LIKE', '%'.$request->get('search').'%');
         }
 
         $helpdesks = $helpdesks->orderBy('name', 'ASC')->paginate(50);
@@ -225,11 +183,10 @@ class HelpdesksController extends Controller
         // they may not have a ->name value but we want to display something anyway
         foreach ($helpdesks as $helpdesk) {
             $helpdesk->use_text = $helpdesk->name;
-            $helpdesk->use_image = ($helpdesk->image) ? Storage::disk('public')->url('helpdesks/'.$helpdesk->image, $helpdesk->image) : null;
+            // $helpdesk->use_image = ($helpdesk->image) ? Storage::disk('public')->url('manufacturers/'.$manufacturer->image, $manufacturer->image) : null;
         }
 
         return (new SelectlistTransformer)->transformSelectlist($helpdesks);
 
     }
-
 }
